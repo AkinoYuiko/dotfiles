e24b8f2d0dd5167da30e78298798dec685614387
Civi <civi@rcg.fun>
2026-03-17 08:09:21 +0000

    fix(bob): pwd typo

--- linux/.config/fish/conf.d/bob.fish
+++ linux/.config/fish/conf.d/bob.fish
@@ -1,1 +1,4 @@
-source "/root/.local/share/bob/env/env.fish"
+# Credit: https://github.com/rust-lang/rustup/blob/master/src/cli/self_update/env.fish
+if not contains "~/.local/share/bob/nvim-bin" $PATH
+    set -x PATH "~/.local/share/bob/nvim-bin" $PATH
+end
