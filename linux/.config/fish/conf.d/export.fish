e88853bef87c23c7daffb0aab1dec3eb79561841
Civi <civi@rcg.fun>
2026-03-17 08:02:43 +0000

    feat(fish): let npm follows XDG rules

--- linux/.config/fish/conf.d/export.fish
+++ linux/.config/fish/conf.d/export.fish
@@ -3,7 +3,12 @@ set -x GPG_TTY (tty)
 # set -x LANG "en_US.UTF-8"
 set -x LESS "-R --use-color --ignore-case --jump-target=4 --LONG-PROMPT --no-init --quit-if-one-screen"
 set -x PAGER bat
-set -x XDG_CONFIG_HOME "$HOME/.config"
+# set -x XDG_CONFIG_HOME "$HOME/.config"
 set -x EDITOR nvim
 set -x VISUAL nvim
 set -x SUDO_EDITOR nvim
+# npm follows XDG
+set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
+set -x NPM_CONFIG_CACHE "$XDG_CACHE_HOME/npm"
+set -x NPM_CONFIG_TMP "$XDG_RUNTIME_DIR/npm"
+
