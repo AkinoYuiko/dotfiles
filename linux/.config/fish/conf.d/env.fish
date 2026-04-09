2499721fb1affe1fb11e135bbf7d21a63ff1076d
Civi <civi@rcg.fun>
2026-04-09 17:01:44 +0900

    refactor: rename export.fish

--- linux/.config/fish/conf.d/env.fish
+++ linux/.config/fish/conf.d/env.fish
@@ -12,3 +12,4 @@ set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
 set -x NPM_CONFIG_CACHE "$XDG_CACHE_HOME/npm"
 set -x NPM_CONFIG_TMP "$XDG_RUNTIME_DIR/npm"
 
+set -gx fish_terminal_color_theme dark
