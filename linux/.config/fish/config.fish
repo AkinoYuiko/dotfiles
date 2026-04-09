45c90f41f9dea1bdd02b4ad9e85f6c11694f3b74
Civi <civi@rcg.fun>
2026-04-09 17:03:59 +0900

    fix: fix theme name renamed

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting ""
-    fish_config theme choose 'ayu Dark'
+    fish_config theme choose --color-theme=dark ayu
     starship init fish | source
 end
