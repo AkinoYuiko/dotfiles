143d7d1da7b26b09fea39a58b197e3cb92d41bd5
Civi <civi@rcg.fun>
2026-04-09 17:07:20 +0900

    feat(fish): use everforest theme

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,6 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     set fish_greeting ""
-    fish_config theme choose --color-theme=dark ayu
     starship init fish | source
 end
