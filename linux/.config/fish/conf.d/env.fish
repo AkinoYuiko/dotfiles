aea4c277f0149b9f4d88a2e3136d4a0f4363b049
Civi <civi@rcg.fun>
2026-04-29 01:38:13 +0900

    feat(fish): change default editor to vim

--- linux/.config/fish/conf.d/env.fish
+++ linux/.config/fish/conf.d/env.fish
@@ -4,9 +4,9 @@ set -x GPG_TTY (tty)
 set -x LESS "-R --use-color --ignore-case --jump-target=4 --LONG-PROMPT --no-init --quit-if-one-screen"
 set -x PAGER bat
 # set -x XDG_CONFIG_HOME "$HOME/.config"
-set -x EDITOR nvim
-set -x VISUAL nvim
-set -x SUDO_EDITOR nvim
+set -x EDITOR vim
+set -x VISUAL vim
+set -x SUDO_EDITOR vim
 # npm follows XDG
 set -x NPM_CONFIG_USERCONFIG "$XDG_CONFIG_HOME/npm/config"
 set -x NPM_CONFIG_CACHE "$XDG_CACHE_HOME/npm"
